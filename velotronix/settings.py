6817086f9c23fdb4b8d62a28605d23ab56d14401
Indrek Mannik <119368421+Inc21@users.noreply.github.com>
2023-10-20 11:38:21 +0100

    Add default font awsome, css and js files and test connection

--- velotronix/settings.py
+++ velotronix/settings.py
@@ -91,7 +91,9 @@ ROOT_URLCONF = 'velotronix.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
