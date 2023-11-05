aab2b5c355496f3c3380ce51b23167c0ca9fbbee
Indrek Mannik <119368421+Inc21@users.noreply.github.com>
2023-11-05 11:03:14 +0000

    Install djange crispy bootstrap5 pakage and add it into the settings

--- velotronix/settings.py
+++ velotronix/settings.py
@@ -56,11 +56,17 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'crispy_forms',
+    'crispy_bootstrap5',
 
     # SocialAccount providers
     'allauth.socialaccount.providers.google',
     'allauth.socialaccount.providers.facebook',
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
