ae84566686d05f2691db7f38abfba202b0bb91cf
Indrek Mannik <119368421+Inc21@users.noreply.github.com>
2023-11-15 04:04:18 +0000

    Fix cart aligment on topnav

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 
 
 def cart(request):
@@ -11,11 +12,23 @@ def add_to_cart(request, item_id):
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
+    item_in_cart = item_id in list(cart.keys())
+    item_quantity = cart
 
-    if item_id in list(cart.keys()):
-        cart[item_id] += quantity
+    if item_in_cart and cart[item_id] >= 90 or quantity > 90:
+        print(item_quantity)
+        messages.warning(request, 'You can only add 99 items per product!')
     else:
-        cart[item_id] = quantity
+        if item_id in list(cart.keys()):
+            cart[item_id] += quantity
+        else:
+            cart[item_id] = quantity
+        if quantity == 1:
+            print(cart[item_id])
+            messages.success(request, f'{ quantity } item added to your cart!')
+            print(quantity)
+        else:
+            messages.success(request, f"{ quantity } item's added to your cart!")
 
     request.session['cart'] = cart
     return redirect(redirect_url)
