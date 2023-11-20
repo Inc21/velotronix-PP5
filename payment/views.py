911e62d0e60af862c2e23148f064ee282c40c170
Indrek Mannik <119368421+Inc21@users.noreply.github.com>
2023-11-20 20:53:07 +0000

    Add Stripe js and css

--- payment/views.py
+++ payment/views.py
@@ -14,5 +14,7 @@ def checkout(request):
     template = 'payment/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_VN0gXSHXckk2l1RI9kWHMEMq',
+        'client_secret': 'test client secret',
     }
     return render(request, template, context)
