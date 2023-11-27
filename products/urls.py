d967a8fd9f0a8df114b10e18540a01f4551a6f4a
Indrek Mannik <119368421+Inc21@users.noreply.github.com>
2023-11-27 12:09:52 +0000

    Add product admin page

--- products/urls.py
+++ products/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path("", views.all_products, name="products"),
     path("<int:product_id>/", views.product_detail, name="product_detail"),
     path("add/", views.add_product, name="add_product"),
+    path("edit/<int:product_id>/", views.edit_product, name="edit_product"),
+    path('product_admin/', views.product_admin, name='product_admin'),
 ]
--- products/views.py
+++ products/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.db.models.functions import Lower
 from django.contrib import messages
 from django.db.models import Q
+from django.contrib.auth.decorators import login_required
+
 
 from .models import Product, Category
 from .forms import ProductForm
@@ -104,6 +106,19 @@ def product_detail(request, product_id):
     return render(request, 'products/product_detail.html', context)
 
 
+@login_required
+def product_admin(request):
+    """
+    A view to show all products with admin controls"""
+    products = Product.objects.all()
+    template = "products/product_admin.html"
+    context = {
+        'products': products,
+    }
+    return render(request, template, context)
+
+
+@login_required
 def add_product(request):
     """
     Add a product to the store
@@ -125,4 +140,37 @@ def add_product(request):
     context = {
         "form": form,
     }
+    return render(request, template, context)
+
+
+@login_required
+def edit_product(request, product_id):
+    """ Edit a product in the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
+    product = get_object_or_404(Product, pk=product_id)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.success(
+                request, f'Successfully updated {product.name}!')
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.warning(
+                request,
+                f'Failed to update {product.name}. Please\
+                    ensure the form is valid.')
+    else:
+        form = ProductForm(instance=product)
+        messages.info(request, f'You are editing {product.name}')
+
+    template = 'products/edit_product.html'
+    context = {
+        'form': form,
+        'product': product,
+    }
+
     return render(request, template, context)
