dc5d9c2081b1b9d91582894b8072138ae6a7ff8e
Indrek Mannik <119368421+Inc21@users.noreply.github.com>
2023-11-28 03:04:54 +0000

    Add templates and some layout to about page

--- about/views.py
+++ about/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
+from .models import About
 
 
 def about(request):
     """ A view to return the about page """
+    about = About.objects.all()
+    delivery = about[0].delivery_info
+    returns = about[0].returns_info
+    faq = about[0].faq
+    privacy = about[0].privacy_policy
 
-    return render(request, "about/about.html")
+    template = "about/about.html"
+    context = {
+        'delivery': delivery,
+        'returns': returns,
+        'faq': faq,
+        'privacy': privacy,
+    }
+
+    return render(request, template, context)
