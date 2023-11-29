0c8aee31ca7c4faf5e4094452c312f246f1b6636
Indrek Mannik <119368421+Inc21@users.noreply.github.com>
2023-11-29 00:21:38 +0000

    Add embeded contact form to about page and connect it with gmail

---
+++ about/forms.py
@@ -0,0 +1,65 @@
+from django import forms
+from .models import About, Contact
+from django_summernote.widgets import SummernoteWidget
+
+
+class AboutForm(forms.ModelForm):
+    """ About form """
+    class Meta:
+        model = About
+        fields = ('delivery_info', 'returns_info', 'faq', 'privacy_policy')
+        widgets = {
+            'description': SummernoteWidget(),
+            'specs': SummernoteWidget(),
+        }
+
+    def __init__(self, *args, **kwargs):
+        """ Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'delivery_info': 'Delivery information',
+            'returns_info': 'Returns information',
+            'faq': 'FAQ',
+            'privacy_policy': 'Privacy policy',
+        }
+
+        self.fields['about'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if field != 'about':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
+                self.fields[field].widget.attrs['class'] = (
+                    'border rounded-1 profile-form-input')
+            self.fields[field].label = False
+
+
+class ContactForm(forms.ModelForm):
+    """
+    This class is used to create a form for the ContactForm model.
+    """
+    class Meta:
+        model = Contact
+        fields = ['author', 'email', 'subject', 'message']
+        labels = {
+            'author': '',
+            'email': '',
+            'subject': '',
+            'message': ''
+        }
+        widgets = {
+          'author': forms.TextInput(attrs={'placeholder': 'Name *'}),
+          'email': forms.EmailInput(attrs={'placeholder': 'Email *'}),
+          'subject': forms.TextInput(attrs={'placeholder': 'Subject *'}),
+          'message': forms.Textarea(attrs={
+              'rows': 4, 'placeholder': 'Message *'}),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(ContactForm, self).__init__(*args, **kwargs)
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'form-control'})
--- about/views.py
+++ about/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import About
+from .forms import ContactForm, AboutForm
+from django.template.loader import render_to_string
+from django.contrib import messages
+from django.core.mail import send_mail
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 
 def about(request):
@@ -10,12 +16,89 @@ def about(request):
     faq = about[0].faq
     privacy = about[0].privacy_policy
 
+    form = ContactForm()
+    author = ""
+    email = ""
+    subject = ""
+    message = ""
+
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        user = request.user
+        if form.is_valid():
+            author = form.cleaned_data['author']
+            email = form.cleaned_data['email']
+            subject = form.cleaned_data['subject']
+            message = form.cleaned_data['message']
+            form.save()
+
+            html = render_to_string('about/emails/contact_form.html', {
+                'author': author,
+                'email': email,
+                'subject': subject,
+                'message': message,
+                'user': user,
+            })
+
+            send_mail('The contact form entry',
+                      'The contact form message',
+                      'noreply@test.com', ['intc21@gmail.com'],
+                      fail_silently=False, html_message=html)
+            messages.success(request, 'Message sent successfully!')
+            return redirect("about")
+
+    else:
+        form = ContactForm()
+
     template = "about/about.html"
     context = {
         'delivery': delivery,
         'returns': returns,
         'faq': faq,
         'privacy': privacy,
+        'form': form,
+        'author': author,
+        'email': email,
+        'subject': subject,
+        'message': message,
     }
 
     return render(request, template, context)
+
+
+# def contactUs(request):
+#     """
+#     This view will display the contact form.
+#     """
+#     if request.method == 'POST':
+#         form = ContactForm(request.POST)
+#         tm_email = request.user.userprofile.email
+#         tm_profile = request.user.userprofile.username
+#         if form.is_valid():
+#             author = form.cleaned_data['author']
+#             email = form.cleaned_data['email']
+#             subject = form.cleaned_data['subject']
+#             message = form.cleaned_data['message']
+
+#             html = render_to_string('users/emails/contact_developer.html', {
+#                 'author': author,
+#                 'email': email,
+#                 'subject': subject,
+#                 'message': message,
+#                 'tm_profile': tm_profile,
+#                 'tm_email': tm_email,
+#             })
+
+#             send_mail('The contact form entry',
+#                       'The contact form message',
+#                       'noreply@test.com', ['intc21@gmail.com'],
+#                       fail_silently=False, html_message=html)
+#             messages.success(request, 'Message sent successfully!')
+#             # next = request.GET.get('next', reverse('contact-form'))
+#             # return HttpResponseRedirect(next)
+#     else:
+#         form = ContactForm()
+
+#     template = "about/about.html"
+#     context = {'form': form}
+#     return render(request, template, context)
