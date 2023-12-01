80b5f0e8d266ad9940e8328e2aad134f2ac65837
Indrek Mannik <119368421+Inc21@users.noreply.github.com>
2023-12-01 01:53:18 +0000

    Add aws cache control

--- velotronix/settings.py
+++ velotronix/settings.py
@@ -220,6 +220,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 if "USE_AWS" in os.environ:
+    # Cache Control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'velotronix'
     AWS_S3_REGION_NAME = 'eu-west-1'
