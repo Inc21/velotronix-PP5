8fd7a2dd8b0563c8b8c18d5bb1af3fb2b862ee33
Indrek Mannik <119368421+Inc21@users.noreply.github.com>
2023-12-02 02:04:16 +0000

    Add custom 500 page

--- velotronix/urls.py
+++ velotronix/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import handler404
+from .views import handler404, handler500
 
 urlpatterns = [
     path('accounts/', include("allauth.urls")),
@@ -32,4 +32,5 @@ urlpatterns = [
     path('about/', include("about.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-handler404 = 'velotronix.views.handler404' # noqa: F811
+handler404 = 'velotronix.views.handler404'  # noqa: F811
+handler500 = 'velotronix.views.handler500'  # noqa: F811
