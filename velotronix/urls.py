eeacb4d43d81d46a89dafb510df87c897646cc14
Indrek Mannik <119368421+Inc21@users.noreply.github.com>
2023-12-02 02:46:39 +0000

    Add custon 403 and 400 page

--- velotronix/urls.py
+++ velotronix/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import handler404, handler500
+from .views import handler404, handler500, handler403, handler400
 
 urlpatterns = [
     path('accounts/', include("allauth.urls")),
@@ -34,3 +34,5 @@ urlpatterns = [
 
 handler404 = 'velotronix.views.handler404'  # noqa: F811
 handler500 = 'velotronix.views.handler500'  # noqa: F811
+handler403 = 'velotronix.views.handler403'  # noqa: F811
+handler400 = 'velotronix.views.handler400'  # noqa: F811
--- velotronix/views.py
+++ velotronix/views.py
@@ -9,3 +9,13 @@ def handler404(request, exception):
 def handler500(request):
     """ Error Handler 500 - Internal Server Error """
     return render(request, "errors/500.html", status=500)
+
+
+def handler403(request, exception):
+    """ Error Handler 403 - Forbidden """
+    return render(request, "errors/403.html", status=403)
+
+
+def handler400(request, exception):
+    """ Error Handler 400 - Bad Request """
+    return render(request, "errors/400.html", status=400)
