268c4dbab9568b3f3b5915b47cf3a6f4356f369c
Indrek Mannik <119368421+Inc21@users.noreply.github.com>
2024-02-28 23:01:53 +0000

    Add PKCE support for Google OAuth provider

--- velotronix/settings.py
+++ velotronix/settings.py
@@ -91,14 +91,14 @@ MIDDLEWARE = [
 
 SOCIALACCOUNT_PROVIDERS = {
     'google': {
-        # For each OAuth based provider, either add a ``SocialApp``
-        # (``socialaccount`` app) containing the required client
-        # credentials, or list them here:
-        'APP': {
-            'client_id': '123',
-            'secret': '456',
-            'key': ''
-        }
+        'SCOPE': [
+            'profile',
+            'email',
+        ],
+        'AUTH_PARAMS': {
+            'access_type': 'online',
+        },
+        'OAUTH_PKCE_ENABLED': True,
     }
 }
 
